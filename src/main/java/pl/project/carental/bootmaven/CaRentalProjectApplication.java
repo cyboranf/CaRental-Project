30d9bb702177c195b2412d3fc393e57868421a84
cyboranf <cyboranf@gmail.com>
2022-09-21 16:16:34 +0200

    Spring boot -configured

---
+++ src/main/java/pl/project/carental/bootmaven/CaRentalProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.project.carental.bootmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaRentalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaRentalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/project/carental/CaRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.carental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaRentalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
