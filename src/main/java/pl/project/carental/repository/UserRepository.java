88d4f687ddc5df3e78b77577f98e760fd2774c59
cyboranf <cyboranf@gmail.com>
2022-09-26 16:56:10 +0200

    User Service -Done

--- src/main/java/pl/project/carental/repository/UserRepository.java
+++ src/main/java/pl/project/carental/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.project.carental.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,6 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
 
     User saveAndFlush(User user);
+
+    List<User> findAll();
 
     Optional<User> findAllById(Long id);
 
--- src/main/java/pl/project/carental/service/UserService.java
+++ src/main/java/pl/project/carental/service/UserService.java
@@ -1,4 +1,35 @@
 package pl.project.carental.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.project.carental.domain.User;
+import pl.project.carental.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        userRepository.save(user);
+        return user;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        return userRepository.findAllById(id).orElseThrow();
+    }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
 }
