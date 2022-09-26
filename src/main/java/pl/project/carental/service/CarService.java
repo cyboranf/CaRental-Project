fc4f7e15d904fa9600f4dfdfc0f26c4b0f1e37c5
cyboranf <cyboranf@gmail.com>
2022-09-26 17:00:13 +0200

    CarService -done

--- src/main/java/pl/project/carental/service/CarService.java
+++ src/main/java/pl/project/carental/service/CarService.java
@@ -1,4 +1,36 @@
 package pl.project.carental.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.project.carental.domain.Car;
+import pl.project.carental.domain.User;
+import pl.project.carental.repository.CarRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class CarService {
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car saveCar(Car car){
+        carRepository.save(car);
+        return car;
+    }
+
+    public List<Car> findAll(){
+        return carRepository.findAll();
+    }
+
+    public Car findById(Long id){
+        return carRepository.findAllById(id).orElseThrow();
+    }
+
+    public void delete(Long id){
+        carRepository.deleteById(id);
+    }
 }
