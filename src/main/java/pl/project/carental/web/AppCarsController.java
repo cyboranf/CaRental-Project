2f310e9321d22fe3cd80762caef7229509f65442
cyboranf <cyboranf@gmail.com>
2022-10-03 15:13:35 +0200

    appCarList -done

--- src/main/java/pl/project/carental/web/AppCarsController.java
+++ src/main/java/pl/project/carental/web/AppCarsController.java
@@ -3,7 +3,13 @@ package pl.project.carental.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.project.carental.domain.Car;
 import pl.project.carental.service.CarService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AppCarsController {
@@ -14,7 +20,12 @@ public class AppCarsController {
     }
 
     @GetMapping("/app/car/list")
-    public ModelAndView show() {
+    public ModelAndView show(HttpServletRequest request,
+                             HttpServletResponse response) {
+        List<Car> carList=carService.findAll();
+
+        request.setAttribute("carList",carList);
+
         return new ModelAndView("appCars");
     }
 }
