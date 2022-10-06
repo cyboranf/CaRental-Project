d65164d1453f42795793750f3293520194561b92
cyboranf <cyboranf@gmail.com>
2022-10-06 14:50:25 +0200

    Logout -done

--- src/main/java/pl/project/carental/web/LogoutController.java
+++ src/main/java/pl/project/carental/web/LogoutController.java
@@ -4,10 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LogoutController {
-    @GetMapping("/logout")
-    public ModelAndView show(){
+    @GetMapping("/app/logout")
+    public ModelAndView show(HttpServletRequest request,
+                             HttpServletResponse response){
+        Cookie[] cookies=request.getCookies();
+        HttpSession session= request.getSession();
+        for (Cookie cookie:cookies){
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+
+        }
+
         return new ModelAndView("home");
     }
 }
